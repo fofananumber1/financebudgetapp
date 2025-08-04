3e9d6fa09c420c772d56d82e2523db42063c289c
fofananumber1 <fofananumber1@vt.edu>
2025-08-04 19:38:31 -0400

    Changed the styling of the homepage slightly

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-50">
-      <h1 className="text-4xl font-bold mb-4">Budget App</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen p-8 bg-white">
+      <h1 className="text-black text-6xl font-bold mb-20">Budget App</h1>
       <p className="text-lg text-gray-700 mb-8">
         Some placeholder text here. This is the homepage for this app, there will be a place to upload CSV
         files and input expense data soon!
