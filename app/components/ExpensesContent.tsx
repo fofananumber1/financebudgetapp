8f4933bf3bcabdff199486c193aeaa213c725460
fofananumber1 <fofananumber1@vt.edu>
2025-09-03 13:19:06 -0400

    Database storage

--- app/components/ExpensesContent.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import type { Expense, ExpensesStore } from '@/lib/expenses.types';
-import { localStore } from '@/lib/expenses.local';
