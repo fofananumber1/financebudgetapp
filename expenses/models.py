e2d5a45efc2455cd9ecba08cddb89504bf7eff6d
fofananumber1 <fofananumber1@vt.edu>
2025-09-03 14:07:12 -0400

    Initialized basic settings for backend development and began implementing data models to be used

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Expense(models.Model):
+    date = models.DateField() #date of expense, YYYY-MM-DD
+    category = models.CharField(max_length=100) #category of expense
+    amount = models.DecimalField(max_digits=10, decimal_places=2) #amount of expense
+    description = models.CharField(max_length=255, blank=True) #optional description
+    created_at = models.DateTimeField(auto_now_add=True) #timestamp for server
+
+class Meta:
+    ordering = ["-date", "-id"] #newest expenses first
+    indexes = [
+        models.Index(fields=["date"]),
+        models.Index(fields=["category"]),
+    ]
+
+def __str__(self):
+    return f"{self.date} {self.category} ${self.amount}"
--- finance_backend/settings.py
+++ finance_backend/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'expenses',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
 ]
 
 ROOT_URLCONF = 'finance_backend.urls'
